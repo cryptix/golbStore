7bb38ec65fa70e464b15f7b0bdbb9af371120aa9
Henry <cryptix@riseup.net>
2014-05-22 23:51:39 +0200

    removed martini/binding and Validate()

--- blog.go
+++ blog.go
@@ -1,10 +1,8 @@
 package golbStore
 
 import (
-	"net/http"
 	"time"
 
-	"github.com/martini-contrib/binding"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 )
@@ -12,33 +10,12 @@ import (
 // struct for dbquery results
 // mgo requires the string literal tags
 type Entry struct {
-	ObjId   bson.ObjectId `bson:"_id,omitempty" form:"-"`
-	Id      string        `bson:"-" form:"id"`
-	Author  string        `bson:"author,omitempty" form:"-"`
-	Written time.Time     `bson:"written,omitempty" form:"-"`
-	Title   string        `bson:"title" form:"title" binding:"required"`
-	Text    string        `bson:"text" form:"text" binding:"required"`
-}
-
-func (e Entry) Validate(errors binding.Errors, req *http.Request) binding.Errors {
-
-	if len(e.Title) == 0 {
-		errors = append(errors, binding.Error{
-			FieldNames:     []string{"title"},
-			Classification: "IncompleteError",
-			Message:        "Title can't be empty",
-		})
-	}
-
-	if len(e.Text) == 0 {
-		errors = append(errors, binding.Error{
-			FieldNames:     []string{"text"},
-			Classification: "IncompleteError",
-			Message:        "Text can't be empty",
-		})
-	}
-
-	return errors
+	ObjId   bson.ObjectId `bson:"_id,omitempty"`
+	Author  string        `bson:"author,omitempty"`
+	Written time.Time     `bson:"written,omitempty"`
+	Title   string        `bson:"title"`
+	Text    string        `bson:"text"`
+	Teaser  string        `bson:"teaser"`
 }
 
 // defaults
